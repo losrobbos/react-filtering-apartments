7c494d58110ca2797da3e583ce01ec25d736c5f4
losrobbos <robert.ristock@googlemail.com>
2025-02-05 23:59:53 +0100

    Made apartments view bit more mobile friendly

--- src/components/FlatList.jsx
+++ src/components/FlatList.jsx
@@ -1,6 +1,25 @@
+import { useEffect } from "react"
 import FlatCard from "./FlatCard"
+import { gsap } from "gsap";
+import { Flip } from "gsap/Flip";
+
+// register plugin globally
+gsap.registerPlugin(Flip);
 
 export const FlatList = ({ flats }) => {
+
+  // TODO: whenever cards changed => apply GSAP flip!
+  // PROBLEM: we cannot hook into the BEFORE and AFTER State with React
+  // TODO: explore useGSAP hook 
+  useEffect(() => {
+    const cards = document.querySelectorAll(".flat-card")
+
+    if(cards.length) {
+      const cardsState = Flip.getState(cards)
+    }
+
+  }, [flats])
+
   return (
     <div className="flat-list">
       {flats.map((flat) => (
